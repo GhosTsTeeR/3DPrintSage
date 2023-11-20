42ab5b7c43b0126806a1eade33cef6c09a38f22e
Ghost <80296035+GhosTsTeeR@users.noreply.github.com>
2023-11-19 19:51:19 -0500

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, jsonify, request, json
 from flask_cors import CORS
 import processor
 import consulta
-from bd import data_user, modify_data_user, add_data_curse, data_curse, data_all_curses
+from bd import data_user, modify_data_user, add_data_curse, data_curse, data_all_curses, data_curse_user, add_data_curse_user, modify_data_curse_user, data_curse_info_user, add_finalizate_curse_user
+
 
 app = Flask(__name__)
 CORS(app)
@@ -112,6 +113,86 @@ def get_response_data_curse_all():
 
     if status == 200:
         return jsonify({"message": "cursos encontrados con exito", "data": data})
+    elif status == 401:
+        return jsonify({"error": "Credenciales inválidas"}), 401
+    else:
+        return jsonify({"error": "Error en el servidor"}), 500
+@app.route('/data-curse-user/<id>', methods=["GET"])
+def get_response_data_curse_user(id):
+    user_id = request.args.get('userId')
+    
+    if id is None:
+        return jsonify({"error": "id del curso es un dato requerido"}), 400
+    if user_id is None:
+        return jsonify({"error": "id del curso es un dato requerido"}), 400
+
+    status, data = data_curse_user(id, user_id)
+
+    if status == 200:
+        return jsonify({"message": "curso encontrado con exito", "data": data})
+    elif status == 401:
+        return jsonify({"error": "Credenciales inválidas"}), 401
+    else:
+        return jsonify({"error": "Error en el servidor"}), 500
+@app.route('/add-data-curse-user/<uid>', methods=["POST"])
+def add_response_data_user(uid):
+    data_json = request.json
+    id = data_json.get('id')
+    name_curse = data_json.get('name')
+    if uid is None:
+        return jsonify({"error": "Uid es un dato requerido"}), 400
+
+    status, data = add_data_curse_user(id, uid, name_curse)
+
+    if status == 200:
+        return jsonify({"message": "Se almaceno correctamente", "data": data})
+    elif status == 401:
+        return jsonify({"error": "Credenciales inválidas"}), 401
+    else:
+        return jsonify({"error": "Error en el servidor"}), 500
+@app.route('/modify-data-curse-user/<uid>', methods=["POST"])
+def modify_response_data_curse_user(uid):
+    data_json = request.json
+    id = data_json.get('id')
+    position = data_json.get('position')
+    estado = data_json.get('estado')
+    if uid is None:
+        return jsonify({"error": "Uid es un dato requerido"}), 400
+
+    status, data = modify_data_curse_user(id, uid, position, estado)
+
+    if status == 200:
+        return jsonify({"message": "Se almaceno correctamente", "data": data})
+    elif status == 401:
+        return jsonify({"error": "Credenciales inválidas"}), 401
+    else:
+        return jsonify({"error": "Error en el servidor"}), 500
+@app.route('/get-curse-info-user/<uid>', methods=["GET"])
+def get_cuerse_info_user(uid):
+    
+    if uid is None:
+        return jsonify({"error": "id del curso es un dato requerido"}), 400
+
+    status, data = data_curse_info_user(uid)
+
+    if status == 200:
+        return jsonify({"message": "curso encontrado con exito", "data": data})
+    elif status == 401:
+        return jsonify({"error": "Credenciales inválidas"}), 401
+    else:
+        return jsonify({"error": "Error en el servidor"}), 500
+@app.route('/add-finalizate-curse-user/<uid>', methods=["POST"])
+def add_response_finalizate_curse_user(uid):
+    data_json = request.json
+    id = data_json.get('id')
+    estate_curse = data_json.get('stateCurse')
+    if uid is None:
+        return jsonify({"error": "Uid es un dato requerido"}), 400
+
+    status, data = add_finalizate_curse_user(id, uid, estate_curse)
+
+    if status == 200:
+        return jsonify({"message": "Se almaceno correctamente", "data": data})
     elif status == 401:
         return jsonify({"error": "Credenciales inválidas"}), 401
     else:
